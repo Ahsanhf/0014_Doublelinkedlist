a3ba475562333dc4d9f3df598c9d434b5683d0f9
ahsanhf <ahsanhf@gmail.com>
2024-05-04 11:03:08 +0700

    memasukkan node baru di daftar

--- 0014_Doublelinkedlist.cpp
+++ 0014_Doublelinkedlist.cpp
@@ -18,3 +18,8 @@ void addNode()
 	cout << "\nEnter the name of the student: ";
 	cin >> newNode->noMhs; // Assign value to the data field of the new node
 	cout << "\nEnter the name of the student: ";
+	cin >> newNode->name; // Assign value to the data field of the new node
+
+	//insert the new node in the list
+	if (START == NULL || newNode->noMhs <= START->noMhs)
+	{ // Step 2: insert the new node at the beginning
